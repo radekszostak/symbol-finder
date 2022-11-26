6bf06f3855453831375ec87038cdbaac8e5dd68c
Radoslaw Szostak <radoslawszostak@gmail.com>
2022-11-27 00:58:23 +0100

    gtignpre

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 rmdir /s /q dist
-pyinstaller --hidden-import 'pystray._win32' --onefile --noconsole symbol-finder.py
+pyinstaller --hidden-import pystray._win32 --hidden-import pynput.keyboard._win32 --onefile --noconsole symbol-finder.py --icon icon.ico
 rmdir /s /q __pycache__
 rmdir /s /q build
 del symbol-finder.spec
 copy /y symbols.csv dist\symbols.csv
+copy /y icon.ico dist\icon.ico
